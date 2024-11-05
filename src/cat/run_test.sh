d72c7ab05106e163365f1262d55bd976dd41a7fc
moon <undruee@gmail.com>
2024-11-05 15:27:56 +0300

    finel tests for cat

--- src/cat/run_test.sh
+++ src/cat/run_test.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+!/bin/bash
 
 MY_CAT="../../build/cat"
 ORIGINAL_CAT="/bin/cat"
 
-TEST_FILES=("text_for_test/test_1.txt" "text_for_test/test_2.txt" "text_for_test/test_3.txt" "text_for_test/test_4.txt" "text_for_test/test_5.txt")
+TEST_FILES=("text_for_test/test_1.txt" "text_for_test/test_2.txt" "text_for_test/test_3.txt" "text_for_test/test_4.txt" "text_for_test/test_5.txt" "")
 
 MATCH_COUNT=0
 TOTAL_TEST=2000
 
-FLAGS=("-b" "-n" "-e" "-E" "-t" "-T" "-s" "-v" "--number-nonblank" "--number" "--squeeze-blank")
+FLAGS=("-b" "-n" "-e" "-E" "-t" "-T" "-s" "-v" "--number-nonblank" "--number" "--squeeze-blank" "")
 
 run_test() {
     local file=$1
     local flag=$2
     original_output=$($ORIGINAL_CAT $flag "$file")
     my_output=$($MY_CAT $flag "$file")
+    echo $original_output
+    echo $my_output
     if [ "$original_output" == "$my_output" ]; then
         MATCH_COUNT=$((MATCH_COUNT+1))
     fi
